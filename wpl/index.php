850eed170bd79abf80c719eca3b6e504a82c3c77
Andrea Fiorucci <andrea.fiorucci@outlook.it>
2026-06-03 15:32:07 +0200

    fix: correct SQL direction and close connection properly in index.php
    The WHERE clause used <= instead of >= so the default view was showing
    records older than 3 months rather than the most recent ones.
    Also pass \$link to mysqli_close(), escape output with htmlspecialchars(),
    and fix the see_all check to use strict string comparison.

--- wpl/index.php
+++ wpl/index.php
@@ -1,59 +1,51 @@
 <?php
+include("connect.php");
 
-	include("connect.php");
+$link    = Connection();
+$see_all = isset($_GET['all']) && $_GET['all'] === 'true';
 
-	$link=Connection();
-	$see_all = $_GET['all'] == true ? true : false;
-
-	if ($see_all) {
-		$result=mysqli_query($link, "SELECT id,distanza,volume_residuo,data_misurazione FROM wpl ORDER BY data_misurazione DESC");
-	}
-	else {
-		$result=mysqli_query($link, "SELECT id,distanza,volume_residuo,data_misurazione FROM wpl WHERE data_misurazione <= (NOW() - INTERVAL 3 MONTH) ORDER BY data_misurazione DESC");
-	}
+if ($see_all) {
+    $result = mysqli_query($link, "SELECT id, distanza, volume_residuo, data_misurazione FROM wpl ORDER BY data_misurazione DESC");
+} else {
+    $result = mysqli_query($link, "SELECT id, distanza, volume_residuo, data_misurazione FROM wpl WHERE data_misurazione >= (NOW() - INTERVAL 3 MONTH) ORDER BY data_misurazione DESC");
+}
 ?>
-
 <html>
-   <head>
-      <title>Sensor Data</title>
-   </head>
+<head>
+    <title>Sensor Data</title>
+</head>
 <body>
-   <h1>Water Level</h1>
+    <h1>Water Level</h1>
 
-   	<?php
-	if ($see_all)
-		echo '<span><b>All records</b></span>';
-	else
-		echo '
-			<div>
-			<span><b>Last 3 months</b></span>
-			&nbsp;&nbsp;
-			<a href="https://fjordoprj.altervista.org/wpl/index.php?all=true">See All</a>
-			</div>
-		';
-   	?>
+    <?php if ($see_all): ?>
+        <span><b>All records</b></span>
+    <?php else: ?>
+        <div>
+            <span><b>Last 3 months</b></span>
+            &nbsp;&nbsp;
+            <a href="index.php?all=true">See All</a>
+        </div>
+    <?php endif; ?>
 
-	<br>
+    <br>
 
-   <table border="1" cellspacing="1" cellpadding="1">
-		<tr>
-			<td>&nbsp;Measured Distance (cm)&nbsp;</td>
-			<td>&nbsp;Residual Volume (m^3)&nbsp;</td>
-			<td>&nbsp;Timestamp &nbsp;</td>
-		</tr>
-
-      <?php
-
-		  if($result!==FALSE){
-		     while($row = mysqli_fetch_array($result)) {
-		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></tr>",
-		           $row["distanza"], $row["volume_residuo"], $row["data_misurazione"]);
-		     }
-		     mysqli_free_result($result);
-			}
-			mysqli_close();
-      ?>
-
-   </table>
+    <table border="1" cellspacing="1" cellpadding="1">
+        <tr>
+            <td>&nbsp;Measured Distance (cm)&nbsp;</td>
+            <td>&nbsp;Residual Volume (dm³)&nbsp;</td>
+            <td>&nbsp;Timestamp&nbsp;</td>
+        </tr>
+        <?php if ($result !== false): ?>
+            <?php while ($row = mysqli_fetch_array($result)): ?>
+                <tr>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['distanza']); ?>&nbsp;</td>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['volume_residuo']); ?>&nbsp;</td>
+                    <td>&nbsp;<?php echo htmlspecialchars($row['data_misurazione']); ?>&nbsp;</td>
+                </tr>
+            <?php endwhile; ?>
+            <?php mysqli_free_result($result); ?>
+        <?php endif; ?>
+    </table>
 </body>
 </html>
+<?php mysqli_close($link); ?>
